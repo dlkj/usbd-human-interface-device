a7e71399d63a78e26e79ce473724ecd7ac330e94
Daniel Kenyon-Jones <mail@dlkj.co.uk>
2022-01-06 18:33:04 +0000

    inital crate structure and github automation

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Utilities and examples for building USB HID devices
+#![no_std]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
