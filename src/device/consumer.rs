fe04c3ddb29a4fb0fe957e7aec2e6cc92214b543
Daniel Kenyon-Jones <git@dlkj.co.uk>
2022-04-01 11:43:47 +0100

    Remove unused descriptors

--- src/device/consumer.rs
+++ src/device/consumer.rs
@@ -12,22 +12,6 @@ use crate::interface::raw::{RawInterface, RawInterfaceConfig};
 use crate::interface::{InterfaceClass, WrappedInterface, WrappedInterfaceConfig};
 use crate::page::Consumer;
 
-///Consumer control report descriptor - Single `u16` consumer control usage code (2 bytes)
-#[rustfmt::skip]
-pub const SINGLE_CODE_REPORT_DESCRIPTOR: &[u8] = &[
-    0x05, 0x0C, // Usage Page (Consumer),
-    0x09, 0x01, // Usage (Consumer Control),
-    0xA1, 0x01, // Collection (Application),
-    0x75, 0x10, //     Report Size(16)
-    0x95, 0x01, //     Report Count(1)
-    0x15, 0x00, //     Logical Minimum(0)
-    0x26, 0x9C, 0x02, //     Logical Maximum(0x029C)
-    0x19, 0x00, //     Usage Minimum(0)
-    0x2A, 0x9C, 0x02, //     Usage Maximum(0x029C)
-    0x81, 0x00, //     Input (Array, Data, Variable)
-    0xC0, // End Collection
-];
-
 ///Consumer control report descriptor - Four `u16` consumer control usage codes as an array (8 bytes)
 #[rustfmt::skip]
 pub const MULTIPLE_CODE_REPORT_DESCRIPTOR: &[u8] = &[
@@ -50,30 +34,6 @@ pub struct MultipleConsumerReport {
     #[packed_field(ty = "enum", element_size_bytes = "2")]
     pub codes: [Consumer; 4],
 }
-
-// #[rustfmt::skip]
-// pub const HID_CONSUMER_CONTROL_VOL_KNOB_7_ARRAY_REPORT_DESCRIPTOR: &[u8] = &[
-//     0x05, 0x0C, // Usage Page (Consumer),
-//     0x09, 0x01, // Usage (Consumer Control),
-//     0xA1, 0x01, // Collection (Application),
-//                 //
-//     0x09, 0xE0, //     Usage (Volume)
-//     0x15, 0x9C, //     Logical Minimum(-100)
-//     0x25, 0x64, //     Logical Maximum(100)
-//     0x95, 0x01, //     ReportCount(1),
-//     0x75, 0x08, //     ReportSize(8),
-//     0x81, 0x06, //     Input (Data, Variable, Relative)
-//                 //
-//     0x75, 0x08, //     Report Size(8)
-//     0x95, 0x07, //     Report Count(7)
-//     0x15, 0x00, //     Logical Minimum(0)
-//     0x26, 0xF5, 0x00, //     Logical Maximum(0xF5)
-//     0x05, 0x0C, //     Usage Page (Consumer),
-//     0x19, 0x00, //     Usage Minimum(0)
-//     0x2A, 0xF5, 0x00, //     Usage Maximum(0xF5)
-//     0x81, 0x00, //     Input (Array, Data, Variable)
-//     0xC0, // End Collection
-// ];
 
 ///Fixed functionality consumer control report descriptor
 /// 
