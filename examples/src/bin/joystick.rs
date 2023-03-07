eb76a88010e3b10786fa9ada2d9e05af2ee93d64
9names <60134748+9names@users.noreply.github.com>
2023-03-07 23:00:53 +1100

    Add joystick device and example

---
+++ examples/src/bin/joystick.rs
@@ -0,0 +1,152 @@
+#![no_std]
+#![no_main]
+
+use bsp::entry;
+use bsp::hal;
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::digital::v2::*;
+use embedded_hal::prelude::*;
+use fugit::ExtU32;
+use hal::pac;
+use panic_probe as _;
+use usb_device::class_prelude::*;
+use usb_device::prelude::*;
+use usbd_human_interface_device::device::joystick::JoystickReport;
+use usbd_human_interface_device::prelude::*;
+
+use rp_pico as bsp;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    info!("Starting");
+
+    //USB
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut joy = UsbHidClassBuilder::new()
+        .add_interface(
+            usbd_human_interface_device::device::joystick::JoystickInterface::default_config(),
+        )
+        .build(&usb_bus);
+
+    //https://pid.codes
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+        .manufacturer("usbd-human-interface-device")
+        .product("Rusty joystick")
+        .serial_number("TEST")
+        .build();
+
+    //GPIO pins
+    let mut led_pin = pins.gpio13.into_push_pull_output();
+
+    let keys: &[&dyn InputPin<Error = core::convert::Infallible>] = &[
+        &pins.gpio0.into_pull_up_input(),
+        &pins.gpio1.into_pull_up_input(),
+        &pins.gpio2.into_pull_up_input(),
+        &pins.gpio3.into_pull_up_input(),
+        &pins.gpio4.into_pull_up_input(),
+        &pins.gpio5.into_pull_up_input(),
+        &pins.gpio6.into_pull_up_input(),
+        &pins.gpio7.into_pull_up_input(),
+        &pins.gpio8.into_pull_up_input(),
+        &pins.gpio9.into_pull_up_input(),
+        &pins.gpio10.into_pull_up_input(),
+        &pins.gpio11.into_pull_up_input(),
+    ];
+
+    led_pin.set_low().ok();
+
+    let mut report = JoystickReport::default();
+
+    let mut input_count_down = timer.count_down();
+    input_count_down.start(10.millis());
+
+    loop {
+        // Poll the keys every 10ms
+        if input_count_down.wait().is_ok() {
+            report = update_report(report, keys);
+
+            match joy.interface().write_report(&report) {
+                Err(UsbHidError::WouldBlock) => {}
+                Ok(_) => {
+                    report = JoystickReport::default();
+                }
+                Err(e) => {
+                    core::panic!("Failed to write joystick report: {:?}", e)
+                }
+            }
+        }
+
+        if usb_dev.poll(&mut [&mut joy]) {}
+    }
+}
+
+fn update_report(
+    mut report: JoystickReport,
+    keys: &[&dyn InputPin<Error = core::convert::Infallible>],
+) -> JoystickReport {
+    // Read out 8 buttons first
+    // Clippy wants us to use an iterator here but it hurts readability
+    #[allow(clippy::needless_range_loop)]
+    for button in 0..=7 {
+        if keys[button].is_low().unwrap() {
+            report.buttons |= 1 << button;
+        } else {
+            report.buttons &= 0xFF - (1 << button);
+        }
+    }
+
+    // We're using digital switches in a D-PAD style configuration
+    //    10
+    //  8    9
+    //    11
+    // These are mapped to the limits of an axis
+    if keys[8].is_low().unwrap() {
+        report.x = -127; // left
+    } else if keys[9].is_low().unwrap() {
+        report.x = 127; // right
+    } else {
+        report.x = 0; // center
+    }
+    if keys[10].is_low().unwrap() {
+        report.y = -127; // up
+    } else if keys[11].is_low().unwrap() {
+        report.y = 127; // down
+    } else {
+        report.y = 0; // center
+    }
+
+    report
+}
---
+++ src/device/joystick.rs
@@ -0,0 +1,106 @@
+//!HID joystick
+use crate::hid_class::descriptor::HidProtocol;
+use core::default::Default;
+use delegate::delegate;
+use fugit::ExtU32;
+use log::error;
+use packed_struct::prelude::*;
+use usb_device::bus::{InterfaceNumber, StringIndex, UsbBus};
+use usb_device::class_prelude::DescriptorWriter;
+
+use crate::hid_class::prelude::*;
+use crate::interface::raw::{RawInterface, RawInterfaceConfig};
+use crate::interface::{InterfaceClass, WrappedInterface, WrappedInterfaceConfig};
+use crate::UsbHidError;
+
+#[rustfmt::skip]
+pub const JOYSTICK_DESCRIPTOR: &[u8] = &[
+    0x05, 0x01, // Usage Page (Generic Desktop)         5,   1
+    0x09, 0x04, // Usage (Joystick)                     9,   4
+    0xa1, 0x01, // Collection (Application)             161, 1
+    0x09, 0x01, //   Usage Page (Pointer)               9,   1
+    0xa1, 0x00, //   Collection (Physical)              161, 0
+    0x09, 0x30, //     Usage (X)                        9,   48
+    0x09, 0x31, //     Usage (Y)                        9,   49
+    0x15, 0x81, //     Logical Minimum (-127)           21,  129
+    0x25, 0x7f, //     Logical Maximum (127)            37,  127
+    0x75, 0x08, //     Report Size (8)                  117, 8
+    0x95, 0x02, //     Report count (2)                 149, 2,
+    0x81, 0x02, //     Input (Data, Variable, Absolute) 129, 2,
+    0xc0,       //   End Collection                     192,
+    0x05, 0x09, //   Usage Page (Button)                5,   9,
+    0x19, 0x01, //   Usage Minimum (0)                  25,  1,
+    0x29, 0x08, //   Usage Maximum (8)                  41,  8,
+    0x15, 0x00, //   Logical Minimum (0)                21,  0
+    0x25, 0x01, //   Logical Maximum (1)                37,  1,
+    0x75, 0x01, //   Report Size (1)                    117, 1,
+    0x95, 0x08, //   Report Count (8)                   149, 8
+    0x81, 0x02, //   Input (Data, Variable, Absolute)   129, 2,
+    0xc0,       // End Collection                       192
+];
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Default, PackedStruct)]
+#[packed_struct(endian = "lsb", size_bytes = "3")]
+pub struct JoystickReport {
+    #[packed_field]
+    pub x: i8,
+    #[packed_field]
+    pub y: i8,
+    #[packed_field]
+    pub buttons: u8,
+}
+
+pub struct JoystickInterface<'a, B: UsbBus> {
+    inner: RawInterface<'a, B>,
+}
+
+impl<'a, B: UsbBus> JoystickInterface<'a, B> {
+    pub fn write_report(&self, report: &JoystickReport) -> Result<(), UsbHidError> {
+        let data = report.pack().map_err(|e| {
+            error!("Error packing JoystickReport: {:?}", e);
+            UsbHidError::SerializationError
+        })?;
+        self.inner
+            .write_report(&data)
+            .map(|_| ())
+            .map_err(UsbHidError::from)
+    }
+
+    pub fn default_config() -> WrappedInterfaceConfig<Self, RawInterfaceConfig<'a>> {
+        WrappedInterfaceConfig::new(
+            RawInterfaceBuilder::new(JOYSTICK_DESCRIPTOR)
+                .boot_device(InterfaceProtocol::None)
+                .description("Joystick")
+                .in_endpoint(UsbPacketSize::Bytes8, 10.millis())
+                .unwrap()
+                .without_out_endpoint()
+                .build(),
+            (),
+        )
+    }
+}
+
+impl<'a, B: UsbBus> InterfaceClass<'a> for JoystickInterface<'a, B> {
+    delegate! {
+        to self.inner{
+           fn report_descriptor(&self) -> &'_ [u8];
+           fn id(&self) -> InterfaceNumber;
+           fn write_descriptors(&self, writer: &mut DescriptorWriter) -> usb_device::Result<()>;
+           fn get_string(&self, index: StringIndex, _lang_id: u16) -> Option<&'_ str>;
+           fn reset(&mut self);
+           fn set_report(&mut self, data: &[u8]) -> usb_device::Result<()>;
+           fn get_report(&mut self, data: &mut [u8]) -> usb_device::Result<usize>;
+           fn get_report_ack(&mut self) -> usb_device::Result<()>;
+           fn set_idle(&mut self, report_id: u8, value: u8);
+           fn get_idle(&self, report_id: u8) -> u8;
+           fn set_protocol(&mut self, protocol: HidProtocol);
+           fn get_protocol(&self) -> HidProtocol;
+        }
+    }
+}
+
+impl<'a, B: UsbBus> WrappedInterface<'a, B, RawInterface<'a, B>> for JoystickInterface<'a, B> {
+    fn new(interface: RawInterface<'a, B>, _: ()) -> Self {
+        Self { inner: interface }
+    }
+}
